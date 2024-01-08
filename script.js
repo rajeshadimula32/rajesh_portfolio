75be3eb3ecb885f23f06ade63dc4c32376f515fc
Adimula Rajesh <rajeshadimula@gmail.com>
2024-01-08 12:50:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+  
+function openMenu()
+{
+  const menu = document.querySelector("#hamburger-nav");
+  menu.style.display="flex";
+  // const icon = document.querySelector(".menu-icon");
+  // icon.style.display="none"
+}
+
+function closeMenu()
+{
+  const menu = document.querySelector("#hamburger-nav");
+  menu.style.display="none";
+  // const icon = document.querySelector(".menu-icon");
+  // icon.style.display="block"
+}
